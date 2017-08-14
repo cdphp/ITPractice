59ccde46217ed0c4b3e3e55b123d0619f5b4547d
hongker <xiaok2013@live.com>
2017-08-14 13:47:23 +0800

    更换smtp端口

--- web_backend/src/v1.0/controller/BaseController.go
+++ web_backend/src/v1.0/controller/BaseController.go
@@ -1,10 +1,12 @@
 package controller
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"log"
 	"math"
+	"net"
 	"net/http"
 	"net/smtp"
 	"path/filepath"
@@ -158,6 +160,68 @@ func GetLimit() int {
 	return 10
 }
 
+// Dial return a smtp client
+func Dial(addr string) (*smtp.Client, error) {
+	conn, err := tls.Dial("tcp", addr, nil)
+	if err != nil {
+		log.Println("Dialing Error:", err)
+		return nil, err
+	}
+	//分解主机端口字符串
+	host, _, _ := net.SplitHostPort(addr)
+	return smtp.NewClient(conn, host)
+}
+
+//参考net/smtp的func SendMail()
+//使用net.Dial连接tls(ssl)端口时,smtp.NewClient()会卡住且不提示err
+//len(to)>1时,to[1]开始提示是密送
+func SendMailUsingTLS(addr string, auth smtp.Auth, from string, to []string, msg []byte) (err error) {
+
+	//create smtp client
+	c, err := Dial(addr)
+	if err != nil {
+		log.Println("Create smpt client error:", err)
+		return err
+	}
+	defer c.Close()
+
+	if auth != nil {
+		if ok, _ := c.Extension("AUTH"); ok {
+			if err = c.Auth(auth); err != nil {
+				log.Println("Error during AUTH", err)
+				return err
+			}
+		}
+	}
+
+	if err = c.Mail(from); err != nil {
+		return err
+	}
+
+	for _, addr := range to {
+		if err = c.Rcpt(addr); err != nil {
+			return err
+		}
+	}
+
+	w, err := c.Data()
+	if err != nil {
+		return err
+	}
+
+	_, err = w.Write(msg)
+	if err != nil {
+		return err
+	}
+
+	err = w.Close()
+	if err != nil {
+		return err
+	}
+
+	return c.Quit()
+}
+
 // SendToMail 发送邮件
 func SendToMail(to, subject, body, mailtype string) error {
 	config := model.NewConfig()
@@ -178,7 +242,7 @@ func SendToMail(to, subject, body, mailtype string) error {
 
 	msg := []byte("To: " + to + "\r\nFrom: " + user + ">\r\nSubject: " + subject + "\r\n" + contentType + "\r\n\r\n" + body)
 	sendTo := strings.Split(to, ";")
-	err := smtp.SendMail(host, auth, user, sendTo, msg)
+	err := SendMailUsingTLS(host, auth, user, sendTo, msg)
 	return err
 }
 
